c1da5f01d7f3d8bc6ce076cce302d573a9c0fe76
Sheng-Han Teng <shteng@student.42wolfsburg.de>
2026-02-21 18:32:54 +0100

    add libraries for open malloc

--- rush02.h
+++ rush02.h
@@ -2,11 +2,8 @@
 # define RUSH02_H
 #include <stdio.h>
 #include <unistd.h>
-
-char	*ft_strncpy(char *dest, char *src, unsigned int n);
-char	**ft_split(char *str, char *charset);
-int     ft_strcmp(char *s1, char *s2);
-void	interactive(void);
+#include <stdlib.h>
+#include <fcntl.h>
 
 typedef struct s_dict
 {
@@ -19,5 +16,15 @@ typedef struct s_dict_list
     long long	*number;
     char		*word;
 }				t_dict_list;
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n);
+char	**ft_split(char *str, char *charset);
+int	parse_dict(char *filename, t_dict *tdp);
+int	parse_list_row(char *list_row, t_dict_list *tdlp);
+int	parse_dict_str(char *str, int size, t_dict *tdp);
+int	parse_dict(char *filename, t_dict *tdp);
+int	free_dict(t_dict tdp);
+int	ft_strcmp(char *s1, char *s2);
+void	interactive(void);
 
 #endif
