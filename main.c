1a4394bc83a5636839100267405e306a0658a547
Judith <89136088+Yuudith@users.noreply.github.com>
2026-02-21 15:34:27 +0100

    Update main.c

--- main.c
+++ main.c
@@ -9,14 +9,14 @@ int main_next(char *dict_file_name, char *num_str)
 
 int main(int argc, char **argv)
 {
-    if (argc == 0x2)
-        return main_next("numbers.dict", argv[0x1]);
-    else if (argc == 0x3)
-        return main_next(argv[0x1], argv[0x2]);
+    if (argc == 2)
+        return main_next("numbers.dict", argv[1]);
+    else if (argc == 3)
+        return main_next(argv[1], argv[2]);
     else
     {
-        write(0x1, "Error: Invalid number of arguments.\n", 36);
-        return (0x1);
+        write(1, "Error: Invalid number of arguments.\n", 36);
+        return (1);
     }
-    return (0x0);
+    return (0);
 }
