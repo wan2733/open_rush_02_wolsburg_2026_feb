ad3a5d62b95e0d5e1f9da7324fc2a989db2487a7
Judith <89136088+Yuudith@users.noreply.github.com>
2026-02-21 15:32:00 +0100

    Update ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -16,14 +16,14 @@ int	is_sep(char c, char *charset)
 {
 	int	i;
 
-	i = 0x0;
+	i = 0;
 	while (charset[i])
 	{
 		if (c == charset[i])
-			return (0x1);
-		i += 0x1;
+			return (1);
+		i += 1;
 	}
-	return (0x0);
+	return (0);
 }
 
 char	*split_help(char *str, char *charset, int *i)
@@ -33,22 +33,22 @@ char	*split_help(char *str, char *charset, int *i)
 	int		size;
 
 	while (str[*i] && is_sep(str[*i], charset))
-		*i += 0x1;
-	j = 0x0;
+		*i += 1;
+	j = 0;
 	while (str[*i] && !is_sep(str[*i], charset))
 	{
-		j += 0x1;
-		*i += 0x1;
+		j += 1;
+		*i += 1;
 	}
-	if (j <= 0x0)
-		return (0x0);
-	ca = malloc(j + 0x1);
-	ca[j] = 0x0;
+	if (j <= 0)
+		return (0);
+	ca = malloc(j + 1);
+	ca[j] = 0;
 	size = j;
 	while (j)
 	{
 		ca[size - j] = str[*i - j];
-		j -= 0x1;
+		j -= 1;
 	}
 	return (ca);
 }
@@ -60,26 +60,26 @@ char	**ft_split(char *str, char *charset)
 	char	**r;
 	int		ri;
 
-	i = 0x0;
-	ri = 0x0;
-	r = malloc(0x1000);
+	i = 0;
+	ri = 0;
+	r = malloc(4096);
 	while (str[i])
 	{
 		t = split_help(str, charset, &i);
 		if (t)
 			r[ri++] = t;
 	}
-	r[ri] = 0x0;
+	r[ri] = 0;
 	return (r);
 }
 
 // int main()
 // {
 // 	char** r = ft_split(",,hello,world,,", ",");
-// 	int i = 0x0;
+// 	int i = 0;
 // 	while (r[i])
 // 	{
 // 		printf("%s\n", r[i]);
-// 		i += 0x1;
+// 		i += 1;
 // 	}
 // }
