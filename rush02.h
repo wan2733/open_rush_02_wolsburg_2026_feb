adbb785a995c28ed0b173dec127ee85bf29f6b98
Sheng-Han Teng <shteng@student.42wolfsburg.de>
2026-02-22 14:18:23 +0100

    add english post processing

--- rush02.h
+++ rush02.h
@@ -37,5 +37,7 @@ void	free_str_split(char **strs);
 int run(char *num, t_dict *dict, char **result);
 int divide_big_ints(char *dividend, char *divisor, char **quotient, char **remainder);
 char	*skip_spaces(char *list_row);
+void 	ft_putstr(char *str);
+int	english_post_process(char **str);
 
 #endif
