3955014de046bbcc586acd5d02f11b1b487e5c91
Wan Chu <wchu@student.42wolfsburg.de>
2026-02-22 22:07:44 +0100

    half fix main.c

--- main.c
+++ main.c
@@ -1,58 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wchu <wchu@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/22 22:07:25 by wchu              #+#    #+#             */
+/*   Updated: 2026/02/22 22:07:25 by wchu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "rush02.h"
 
-int print_error(int error_code)
+int	print_error(int error_code)
 {
 	if (error_code == 0)
-		return -1;
+		return (-1);
 	if (error_code < -1)
 		write(1, "Dict Error\n", 11);
 	else
 		write(1, "Error\n", 6);
-	return 0;
+	return (0);
 }
 
-int parse_num_str(char **num_str)
+int	parse_num_str(char **num_str)
 {
-	char	*new_str = skip_spaces_num(*num_str);
+	char	*new_str;
+	int		i;
+
+	new_str = skip_spaces_num(*num_str);
 	*num_str = new_str;
-	int i = 0;
+	i = 0;
 	while (new_str[i])
 	{
 		if (new_str[i] < '0' || new_str[i] > '9')
-			return -1;
+			return (-1);
 		i++;
 	}
-	return 0;
+	return (0);
 }
 
 int	main_next(char *dict_file_name, char *num_str)
 {
 	t_dict	td;
+	char	**result;
+	char	*r;
+
 	td.dict_list = malloc(sizeof(t_dict_list) * 4096);
 	if (!td.dict_list)
-		return -1;
+		return (-1);
 	td.size = 0;
-
 	if (parse_num_str(&num_str))
 	{
 		free_dict(&td);
 		free(num_str);
-		return -1;
+		return (-1);
 	}
 	if (parse_dict(dict_file_name, &td))
 	{
 		free_dict(&td);
 		free(num_str);
-		return -2;
+		return (-2);
 	}
 	sort_dict(&td);
-	char **result;
-	char *r = malloc(1);
-	if (!r) 
+	r = malloc(1);
+	if (!r)
 	{
 		free_dict(&td);
 		free(num_str);
-		return -1;
+		return (-1);
 	}
 	r[0] = 0;
 	result = &r;
@@ -84,8 +100,8 @@ int	main(int argc, char **argv)
 	else if (argc == 3)
 	{
 		ec = main_next(argv[1], argv[2]);
-			if (ec)
-				print_error(ec);
+		if (ec)
+			print_error(ec);
 	}
 	else
 	{
