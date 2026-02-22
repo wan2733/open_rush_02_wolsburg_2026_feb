ebe2e32c9a4a100ec1b1c06149e270c2a7661deb
Sheng-Han Teng <shteng@student.42wolfsburg.de>
2026-02-22 21:43:45 +0100

    norm english.c

--- english.c
+++ english.c
@@ -1,30 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   english.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shteng <shteng@student.42wolfsburg.de      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/22 20:51:37 by shteng            #+#    #+#             */
+/*   Updated: 2026/02/22 21:32:55 by shteng           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "rush02.h"
-/*
-english_post_process post process is for completing the bonus
-Using “-”, “,”, “and” to be closer to the correct written syntax
-
-this input char **str is for example 
-one hundred twenty three million four hundred fifty six thousand seven hundred eighty nine
-after the function finish, char **str is going to be
-one hundred twenty three million, four hundred fifty six thousand, seven hundred eighty and nine
-
-this program will return 0 for sucess, -1 for any error
-
-this program will be called in main.c main_next function
-*/
 
 int	check_word(char *str)
 {
 	int	len;
 
 	len = ft_strlen(str);
-	if (str[len - 2] == 't' && str[len - 1] == 'y') // ...ty
+	if (str[len - 2] == 't' && str[len - 1] == 'y')
 		return (1);
-	else if (str[len - 2] == 'e' && str[len - 1] == 'd') // hundred
+	else if (str[len - 2] == 'e' && str[len - 1] == 'd')
 		return (1);
-	else if (str[len - 2] == 'n' && str[len - 1] == 'd') // thousand
+	else if (str[len - 2] == 'n' && str[len - 1] == 'd')
 		return (1);
-	else if (str[len - 2] == 'o' && str[len - 1] == 'n') // ...ion
+	else if (str[len - 2] == 'o' && str[len - 1] == 'n')
 		return (1);
 	return (0);
 }
@@ -38,9 +37,7 @@ int	english_post_process(char **str)
 	words = ft_split(str[0], " ");
 	n = 0;
 	while (words[n])
-	{
 		n++;
-	}
 	i = 0;
 	while (words[i])
 	{
@@ -50,13 +47,9 @@ int	english_post_process(char **str)
 		if (check_word(words[i]) && i != (n - 1))
 		{
 			if (i == n - 2)
-			{
 				ft_putstr(" and ");
-			}
 			else
-			{
 				ft_putstr(", ");
-			}
 		}
 		i++;
 	}
