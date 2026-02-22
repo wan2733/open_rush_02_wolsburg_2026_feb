1a5ee078a031565f65965599140a5f169e4ca6fd
Sheng-Han Teng <shteng@student.42wolfsburg.de>
2026-02-22 21:54:26 +0100

    norm interactive.c

--- interactive.c
+++ interactive.c
@@ -1,12 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   interactive.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shteng <shteng@student.42wolfsburg.de      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/22 18:06:25 by shteng            #+#    #+#             */
+/*   Updated: 2026/02/22 18:07:58 by shteng           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "rush02.h"
 
-void	interactive()
+void	interactive(void)
 {
 	char	buffer[1024];
 	int		size;
-	
-	while ((size = read(0, buffer, sizeof(buffer))) > 0)
+
+	while (1)
 	{
+		size = read(0, buffer, sizeof(buffer));
 		buffer[size - 1] = 0;
 		if (main_next("numbers.dict", buffer))
 			print_error(-1);
