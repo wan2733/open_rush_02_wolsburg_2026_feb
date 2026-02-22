0cf4aa096b5bde021b11bd3df522f1f93bd98fc8
Wan Chu <wchu@student.42wolfsburg.de>
2026-02-22 22:16:57 +0100

    fixing main_next.c

--- main_next.c
+++ main_next.c
@@ -6,14 +6,17 @@
 /*   By: wchu <wchu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/22 22:07:25 by wchu              #+#    #+#             */
-/*   Updated: 2026/02/22 22:12:06 by wchu             ###   ########.fr       */
+/*   Updated: 2026/02/22 22:15:16 by wchu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rush02.h"
 
-int	main_next_1(t_dict *td, char *r, char	**result, char *num_str)
+int	main_next_1(t_dict *td, char *num_str)
 {
+	char	*r;
+	char	**result;
+
 	sort_dict(td);
 	r = malloc(1);
 	if (!r)
@@ -35,8 +38,6 @@ int	main_next_1(t_dict *td, char *r, char	**result, char *num_str)
 int	main_next(char *dict_file_name, char *num_str)
 {
 	t_dict	td;
-	char	**result;
-	char	*r;
 
 	td.dict_list = malloc(sizeof(t_dict_list) * 4096);
 	if (!td.dict_list)
@@ -54,6 +55,6 @@ int	main_next(char *dict_file_name, char *num_str)
 		free(num_str);
 		return (-2);
 	}
-	main_next_1(&td, r, result, num_str);
+	main_next_1(&td, num_str);
 	return (0);
 }
