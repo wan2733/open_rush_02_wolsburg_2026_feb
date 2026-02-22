7458f200aadb4813c33dc5f65c9aff1e24f60fee
Wan Chu <wchu@student.42wolfsburg.de>
2026-02-22 22:18:30 +0100

    .

--- main.c
+++ main.c
@@ -6,79 +6,27 @@
 /*   By: wchu <wchu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/22 22:07:25 by wchu              #+#    #+#             */
-/*   Updated: 2026/02/22 22:13:23 by wchu             ###   ########.fr       */
+/*   Updated: 2026/02/22 22:18:01 by wchu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rush02.h"
 
-// int	print_error(int error_code)
-// {
-// 	if (error_code == 0)
-// 		return (-1);
-// 	if (error_code < -1)
-// 		write(1, "Dict Error\n", 11);
-// 	else
-// 		write(1, "Error\n", 6);
-// 	return (0);
-// }
+int	main_arg2(char **argv)
+{
+	int	ec;
 
-// int	parse_num_str(char **num_str)
-// {
-// 	char	*new_str;
-// 	int		i;
-
-// 	new_str = skip_spaces_num(*num_str);
-// 	*num_str = new_str;
-// 	i = 0;
-// 	while (new_str[i])
-// 	{
-// 		if (new_str[i] < '0' || new_str[i] > '9')
-// 			return (-1);
-// 		i++;
-// 	}
-// 	return (0);
-// }
-
-// int	main_next(char *dict_file_name, char *num_str)
-// {
-// 	t_dict	td;
-// 	char	**result;
-// 	char	*r;
-
-// 	td.dict_list = malloc(sizeof(t_dict_list) * 4096);
-// 	if (!td.dict_list)
-// 		return (-1);
-// 	td.size = 0;
-// 	if (parse_num_str(&num_str))
-// 	{
-// 		free_dict(&td);
-// 		free(num_str);
-// 		return (-1);
-// 	}
-// 	if (parse_dict(dict_file_name, &td))
-// 	{
-// 		free_dict(&td);
-// 		free(num_str);
-// 		return (-2);
-// 	}
-// 	sort_dict(&td);
-// 	r = malloc(1);
-// 	if (!r)
-// 	{
-// 		free_dict(&td);
-// 		free(num_str);
-// 		return (-1);
-// 	}
-// 	r[0] = 0;
-// 	result = &r;
-// 	run(num_str, &td, result);
-// 	english_post_process(result);
-// 	free(r);
-// 	free_dict(&td);
-// 	free(num_str);
-// 	return (0);
-// }
+	if (ft_strcmp(argv[1], "-") == 0)
+	{
+		interactive();
+	}
+	else
+	{
+		ec = main_next("numbers.dict", argv[1]);
+		if (ec)
+			print_error(ec);
+	}
+}
 
 int	main(int argc, char **argv)
 {
@@ -86,16 +34,7 @@ int	main(int argc, char **argv)
 
 	if (argc == 2)
 	{
-		if (ft_strcmp(argv[1], "-") == 0)
-		{
-			interactive();
-		}
-		else
-		{
-			ec = main_next("numbers.dict", argv[1]);
-			if (ec)
-				print_error(ec);
-		}
+		main_arg2(argv);
 	}
 	else if (argc == 3)
 	{
