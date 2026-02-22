fb5419496dd81d3c2c8e419121ff95a9d8a530c3
Daviti Gvimradze <davitigvimradze@MacBook-von-Daviti.fritz.box>
2026-02-22 09:01:40 +0100

    i switch for loops on while loops

--- num_to_lang.c
+++ num_to_lang.c
@@ -5,69 +5,84 @@
 // Helper: find word in t_dict for a given number
 char *find_word(long long num, t_dict *td)
 {
-    for (unsigned int i = 0; i < td->size; i++)
+    unsigned int i;
+
+    i = 0;
+    while (i < td->size)
     {
         if (*(td->dict_list[i].number) == num)
-            return td->dict_list[i].word;
+            return (td->dict_list[i].word);
+        i++;
     }
-    return NULL;
+    return (NULL);
 }
 
 // Recursive function to print number
 int num_to_lang(long long num, char *lang, t_dict *td)
 {
+    long long scales[4];
+    int i;
+    char *scale_word;
+    char *tens_word;
+    char *unit_word;
+    long long tens;
+
+    (void)lang;
+
     if (num == 0)
     {
         ft_putstr(find_word(0, td));
-        return 0;
+        return (0);
     }
 
-    // handle negative numbers
     if (num < 0)
     {
         ft_putstr("Minus ");
         num = -num;
     }
 
-    // Handle billions, millions, thousands
-    long long scales[] = {1000000000LL, 1000000LL, 1000LL, 100LL};
-    for (int i = 0; i < 4; i++)
+    scales[0] = 1000000000LL;
+    scales[1] = 1000000LL;
+    scales[2] = 1000LL;
+    scales[3] = 100LL;
+
+    i = 0;
+    while (i < 4)
     {
         if (num >= scales[i])
         {
-            long long left = num / scales[i];
-            num_to_lang(left, lang, td); // recursive call for left part
+            num_to_lang(num / scales[i], lang, td);
             ft_putstr(" ");
-            char *scale_word = find_word(scales[i], td);
+            scale_word = find_word(scales[i], td);
             if (scale_word)
             {
                 ft_putstr(scale_word);
                 ft_putstr(" ");
             }
-            num %= scales[i]; // remaining part
+            num = num % scales[i];
         }
+        i++;
     }
 
-    // Handle tens and units
     if (num >= 20)
     {
-        long long tens = (num / 10) * 10;
-        char *tens_word = find_word(tens, td);
+        tens = (num / 10) * 10;
+        tens_word = find_word(tens, td);
         if (tens_word)
         {
             ft_putstr(tens_word);
             ft_putstr(" ");
         }
-        num %= 10;
+        num = num % 10;
     }
 
     if (num > 0)
     {
-        char *unit_word = find_word(num, td);
+        unit_word = find_word(num, td);
         if (unit_word)
             ft_putstr(unit_word);
         ft_putstr(" ");
     }
 
-    return 0;
+    return (0);
 }
